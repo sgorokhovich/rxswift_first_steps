2f43df34a20780ee2487d0b9e9c8f34fad1aec4a
Sergey Gorokhovich <sgorokhovich@sergeys-macbook-pro.local>
2016-07-24 23:54:40 +0300

    Initial Commit

---
+++ RxSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwiftDemo
+//
+//  Created by Sergey Gorokhovich on 7/24/16.
+//  Copyright © 2016 Sergey Gorokhovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
