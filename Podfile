ccc657147b303130cb85b69768a891dda55552bb
Sergey Gorokhovich <sgorokhovich@Sergeys-MacBook-Pro.local>
2016-07-25 05:28:03 +0300

    TextField validation demo

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxSwiftDemo' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+	pod 'RxSwift',    '~> 2.0'
+    	pod 'RxCocoa',    '~> 2.0'
+  # Pods for RxSwiftDemo
+
+  target 'RxSwiftDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+	pod 'RxBlocking', '~> 2.0'
+    	pod 'RxTests',    '~> 2.0'
+  end
+
+end
--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -8,11 +8,55 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
+
+extension String {
+    func isEmail() -> Bool {
+        let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        
+        var isEmail = false
+        
+        do {
+            let regex = try NSRegularExpression(pattern: emailPattern, options: .CaseInsensitive)
+            isEmail = regex.firstMatchInString(self, options: [], range: NSMakeRange(0, characters.count)) != nil
+        }
+        catch {
+            print(error)
+        }
+        
+        return isEmail
+    }
+}
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var submitButton: UIButton!
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let emailTextFieldValid = emailTextField.rx_text.map { text in
+            text.isEmail()
+        }.shareReplay(1)
+        
+        let passwordTextFieldValid = passwordTextField.rx_text.map { text in
+            text.characters.count > 5
+        }.shareReplay(1)
+        
+        let infoValid = Observable.combineLatest(emailTextFieldValid, passwordTextFieldValid) { (email, pass) -> Bool in
+            return email && pass
+        }.shareReplay(1)
+        
+        infoValid.bindTo(submitButton.rx_enabled).addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
